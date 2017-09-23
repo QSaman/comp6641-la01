144cf70d16c265e0dd6b9975326d207a3ce48e37
Saman Saadi <saman@saadi>
2017-09-23 02:21:38 -0400

    Making Asio as standalone.

--- cmake/DownloadAsio.cmake
+++ cmake/DownloadAsio.cmake
@@ -5,6 +5,8 @@
 
 set(ASIO_INSTALL_LOCATION "${PROJECT_BINARY_DIR}/external/asio")
 
+add_definitions(-DASIO_STANDALONE)
+
 include(ExternalProject)
 ExternalProject_Add(external_asio
         URL https://github.com/chriskohlhoff/asio/archive/asio-1-11-0.zip
